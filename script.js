21d850eb5d9dcad2fd4f4b2c5c2afb94f20b03ee
Darren Mei <Darrenmei96@users.noreply.github.com>
2024-09-29 18:22:27 -0400

    Fixed header button

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
+const filterButtons = document.querySelector("#filter-btns").children;
+const items = document.querySelector(".portfolio-gallery").children;
 
 
-
-
-
-
-const filterButtons=document.querySelector("#filter-btns").children;
-const items=document.querySelector(".portfolio-gallery").children;
-
-
-for(let i=0; i<filterButtons.length; i++){
-	filterButtons[i].addEventListener("click",function(){
-		for(let j=0; j<filterButtons.length; j++){
+for (let i = 0; i < filterButtons.length; i++) {
+	filterButtons[i].addEventListener("click", function () {
+		for (let j = 0; j < filterButtons.length; j++) {
 			filterButtons[j].classList.remove("active")
 		}
 		this.classList.add("active");
-		const target=this.getAttribute("data-target")
+		const target = this.getAttribute("data-target")
 
-		for(let k=0; k<items.length; k++){
-			items[k].style.display="none";
-			if(target==items[k].getAttribute("data-id"))
-			{
-				items[k].style.display="block";
+		for (let k = 0; k < items.length; k++) {
+			items[k].style.display = "none";
+			if (target == items[k].getAttribute("data-id")) {
+				items[k].style.display = "block";
 			}
-			if(target=="all"){
-				items[k].style.display="block"
+			if (target == "all") {
+				items[k].style.display = "block"
 			}
 		}
 	})
@@ -32,9 +25,9 @@ for(let i=0; i<filterButtons.length; i++){
 
 
 //header fixed
-
+let previousActive = null;
 window.addEventListener('scroll', () => {
-	const docScrollTop=document.documentElement.scrollTop;
+	const docScrollTop = document.documentElement.scrollTop;
 
 	const homeRect = document.getElementById('home').getBoundingClientRect();
 	const aboutRect = document.getElementById('about').getBoundingClientRect();
@@ -49,47 +42,61 @@ window.addEventListener('scroll', () => {
 		[homeRect, homeNav],
 		[aboutRect, aboutNav],
 		[portfolioRect, portfolioNav],
-		[contactRect, contactNav],
 	];
 
-	if(window.innerWidth>991){
-		if(docScrollTop>100){
-			document.querySelector("header").classList.add("fixed")
+	const header = document.querySelector("header")
+
+	if (window.innerWidth > 991) {	
+		if (docScrollTop > header.scrollHeight) {
+			header.classList.add("fixed")
 		}
-		else{
-			document.querySelector("header").classList.remove("fixed")
+		else {
+			header.classList.remove("fixed")
 		}
 	}
 
-	let previousActive = null;
+
 	for (const [rect, nav] of navbarRefs) {
-		if (rect.top <= window.innerHeight && rect.bottom >= 0) {
-			nav.classList.add('active');
+		if (rect.top <= window.innerHeight && rect.bottom > header.scrollHeight) {
 			if (previousActive) {
 				previousActive.classList.remove('active');
 			}
 			previousActive = nav;
+			nav.classList.add('active');
+			break;
 		} else {
 			nav.classList.remove('active');
 		}
 	}
-	
+
+	if (contactRect.top <= window.innerHeight && contactRect.bottom >= 0) {
+		if (previousActive) {
+			previousActive.classList.remove('active');
+		}
+		previousActive = contactNav;
+		contactNav.classList.add('active');
+	}
+
 });
 
 
 // navbar links
 const navbar = document.querySelector(".navbar");
-	a=navbar.querySelectorAll("a");
+a = navbar.querySelectorAll("a");
 
-	a.forEach(function(element){
-		element.addEventListener("click",function(){
-			for(let i=0; i<a.length; i++){
-				a[i].classList.remove("active");
-			}
-			this.classList.add("active")
-			document.querySelector(".navbar").classList.toggle("show");
-		})
+a.forEach(function (element) {
+	element.addEventListener("click", function () {
+		for (let i = 0; i < a.length; i++) {
+			a[i].classList.remove("active");
+		}
+		if (previousActive) {
+			previousActive.classList.remove('active');
+		}
+		this.classList.add("active")
+		previousActive = this;
+		document.querySelector(".navbar").classList.toggle("show");
 	})
+})
 
 
 //page scroll
@@ -99,9 +106,9 @@ const navbar = document.querySelector(".navbar");
 
 //ham-burger
 
-const hamBurger=document.querySelector(".ham-burger");
+const hamBurger = document.querySelector(".ham-burger");
 
-hamBurger.addEventListener("click",function(){
+hamBurger.addEventListener("click", function () {
 	document.querySelector(".navbar").classList.toggle("show");
 })
 
